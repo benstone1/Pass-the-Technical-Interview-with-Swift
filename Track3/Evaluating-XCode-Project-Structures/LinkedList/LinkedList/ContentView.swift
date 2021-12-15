bc7d24ced2074002852fc9f98244455dd29311ff
Bob Pascazio <bob.pascazio@gmail.com>
2021-12-15 15:47:45 -0500

    Added linked list prodject.

---
+++ Track3/Evaluating-XCode-Project-Structures/LinkedList/LinkedList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LinkedList
+//
+//  Created by Pascazio, Bob (Agoda) on 12/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+private func content() {
+  
+  return
+}
---
+++ Track3/Evaluating-XCode-Project-Structures/LinkedList/LinkedList/LinkedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedListApp.swift
+//  LinkedList
+//
+//  Created by Pascazio, Bob (Agoda) on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LinkedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
