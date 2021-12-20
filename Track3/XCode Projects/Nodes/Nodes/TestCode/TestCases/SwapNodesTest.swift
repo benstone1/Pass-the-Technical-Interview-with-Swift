6f38e64bce581ca3b54f5fbf3bd261f4609d8a50
Bob Pascazio <bob.pascazio@gmail.com>
2021-12-20 07:14:09 -0500

    Fixup test cases and example for swap nodes.

--- Track3/XCode Projects/Nodes/Nodes/TestCode/TestCases/SwapNodesTest.swift
+++ Track3/XCode Projects/Nodes/Nodes/TestCode/TestCases/SwapNodesTest.swift
@@ -43,15 +43,15 @@ class SwapNodesTest: Testable {
       node2Prev?.next = node1
       let tempNode = node1?.next
       node1?.next = node2?.next
-
+      
       if node1Prev == nil {
         list.head = node2
       }
-
+      
       if node2?.next == nil {
         list.tail = node1
       }
-
+      
       node2?.next = tempNode
       
     }
@@ -59,22 +59,33 @@ class SwapNodesTest: Testable {
     let testList1 = LinkedList()
     let testList2 = LinkedList()
     let testList3 = LinkedList()
+    let testList4 = LinkedList()
     for i in 1...10 {
       testList1.append(String(i))
       testList2.append(String(i))
       testList3.append(String(i))
+      testList4.append(String(i))
     }
     print(testList1)
     swapNodes(list: testList1, index1: 2, index2: 5)
     print(testList1)
-    print("tail is \(String(describing: testList1.tail!))")
+    print("tail is \(String(describing: testList1.tail!)) head is \(String(describing: testList1.head!))")
+    
     print(testList2)
     swapNodes(list: testList2, index1: 0, index2: 5)
     print(testList2)
-    print("tail is \(String(describing: testList2.tail!))")
+    print("tail is \(String(describing: testList2.tail!)) head is \(String(describing: testList2.head!))")
+    
     print(testList3)
     swapNodes(list: testList3, index1: 2, index2: 9)
     print(testList3)
-    print("tail is \(String(describing: testList3.tail!))")
+    print("tail is \(String(describing: testList3.tail!)) head is \(String(describing: testList3.head!))")
+    
+    print("edge case not covered")
+    print(testList4)
+    print("tail is \(String(describing: testList4.tail!)) head is \(String(describing: testList4.head!))")
+    swapNodes(list: testList4, index1: 4, index2: 0)
+    print(testList4)
+    print("tail is \(String(describing: testList4.tail!))")
   }
 }
