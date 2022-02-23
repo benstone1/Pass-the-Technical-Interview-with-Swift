ee2c4a67e6cdd0812f20aa609fcf7b8bbb8c993e
Evilsmaher <bsf70344@gmail.com>
2022-02-22 22:26:28 -0500

    Graphs+Bubble+Merge
    Adding Playgrounds for:
    
    - Graphs
    - Bubble Sort
    - Merge Sort
    
    Bubble Sort and Merge Sort are paired with examples.
    
    Graphs Playground has the final codebase at the top with the individual exercises and associated code in sections below with numbers. xa is the pre-code and xb is the solution. For example, 1a is the code the student's will initially see and 1b is the solution code for that exercise.

---
+++ Track5/Bubble Sort/BubbleSort.playground/Contents.swift
@@ -0,0 +1,23 @@
+func bubbleSort(inputArray: inout [Int]) {
+  for i in 0..<inputArray.count {
+    for j in 1..<inputArray.count - i {
+      if inputArray[j] < inputArray[j - 1] {
+        print("Swapping values: \(inputArray[j]) and \(inputArray[j - 1])")
+        inputArray.swapAt(j, j - 1)
+      }
+    }
+  }
+  print("Sorted array: \(inputArray)")
+}
+
+var countBackwards = [10, 8, 3, 1, -5]
+print("Array 1 to sort: \(countBackwards)")
+// Call bubble sort on reverse sorted
+bubbleSort(inputArray: &countBackwards)
+print("Sorted array 1: \(countBackwards)")
+
+var countForwards = [5, 1, 3, 8, 10]
+print("Array 2 to sort: \(countForwards)")
+// Call bubble sort on sorted
+bubbleSort(inputArray: &countForwards)
+print("Sorted array 2: \(countForwards)")
