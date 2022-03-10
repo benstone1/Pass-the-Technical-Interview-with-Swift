e90ed34eca3c6c2573e7b64d4514a8a5e9c56f84
Evilsmaher <bsf70344@gmail.com>
2022-03-09 22:19:57 -0500

    Update Quicksort
    - Made the changes identified in Github
    - Also changed `value` to `index`

--- Track5/Quick Sort/Quick Sort.playground/Contents.swift
+++ Track5/Quick Sort/Quick Sort.playground/Contents.swift
@@ -7,12 +7,12 @@
 func quicksort1(_ array: [Int]) -> [Int] {
   guard array.count > 1 else { return array }
 
-  let pivot = array[array.count/2]
-  let less = array.filter { $0 < pivot }
-  let equal = array.filter { $0 == pivot }
-  let greater = array.filter { $0 > pivot }
+  let pivot = array[array.count / 2]
+  let smallerElements = array.filter { $0 < pivot }
+  let equalElements = array.filter { $0 == pivot }
+  let greaterElements = array.filter { $0 > pivot }
 
-  return quicksort1(less) + equal + quicksort1(greater)
+  return quicksort1(smallerElements) + equalElements + quicksort1(greaterElements)
 }
 
 var array1 = [1, 9, 2, 8, 3, 7, 4, 6, 5]
@@ -24,25 +24,25 @@ print(sorted1)
 // 2.
 func quicksort2(_ array: inout [Int], from start: Int, to end: Int) {
   if start < end {
-    let partititon = partition(&array, from: start, to: end)
-    quicksort2(&array, from: start, to: partititon-1)
-    quicksort2(&array, from: partititon+1, to: end)
+    let partititonIndex = partition(&array, from: start, to: end)
+    quicksort2(&array, from: start, to: partititonIndex-1)
+    quicksort2(&array, from: partititonIndex+1, to: end)
   }
 }
 
 func partition(_ array: inout [Int], from start: Int, to end: Int) -> Int {
   let pivot = array[end]
   
-  var lessThanPivotIncrementer = start
-  for value in start..<end {
-    if array[value] <= pivot {
-      array.swapAt(lessThanPivotIncrementer, value)
-      lessThanPivotIncrementer += 1
+  var currentIndex = start
+  for index in currentIndex..<end {
+    if array[index] <= pivot {
+      array.swapAt(currentIndex, index)
+      currentIndex += 1
     }
   }
   
-  array.swapAt(lessThanPivotIncrementer, end)
-  return lessThanPivotIncrementer
+  array.swapAt(currentIndex, end)
+  return currentIndex
 }
 
 var array2 = [1, 9, 2, 8, 3, 7, 4, 6, 5]
